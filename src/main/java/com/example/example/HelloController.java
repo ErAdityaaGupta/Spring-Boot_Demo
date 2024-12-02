b0a5f879629697ebc058134fecd4636423214d1a
Aditya Gupta <ingenious.adig@gmail.com>
2024-12-02 20:18:12 +0530

    Come basic annotations

---
+++ src/main/java/com/example/example/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public HelloResponse sayHello() {
+        return new HelloResponse("Hello World");
+    }
+
+    @PostMapping("/hello")
+    public HelloResponse sayHelloPost(@RequestBody String name) {
+        return new HelloResponse( "Hello" + name + "!");
+    }
+}
